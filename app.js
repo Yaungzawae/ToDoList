664f7f6dbef16ba47571663ca6742652abacc4c4
Ye Zaw Aung <yezaw07aung@gmail.com>
2023-01-21 21:59:21 +0630

    Add task data submit into the app with ejs template

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { render } = require("ejs");
+// const ejs = require("ejs");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+var data = [];
 
 app.get("/", (req , res)=>{
-    res.send("Hvcvello");
+    var options={
+        weekday: "long",
+        day:"numeric",
+        month:"long"
+    }
+    var today = new Date().toLocaleDateString("en-US", options);
+    res.render("list", {today : today, data:data});
+})
+
+app.post("/", (req, res)=>{
+    var task = req.body.task;
+    data.push(task);
+    res.redirect("/");
 })
 
 app.listen(3000,()=>{
     console.log("Server is running in port 3000");
 })
 
+function dayOfWeekAsString(dayIndex) {
+    return ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][dayIndex] || '';
+  }
